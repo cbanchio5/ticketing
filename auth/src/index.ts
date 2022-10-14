ad164d83de7d415e07bcce239ce54775dfddbf97
cesar banchio <cbanchio5@gmail.com>
2022-10-14 19:44:11 +0200

    ingress deployment file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(json());
 
-app.listen(3000, ()=> {
-  console.log('Listening on 3000')
+app.get('/api/users/currentuser', (req, res) => {
+  res.send("Hi there");
 })
+
+app.listen(3000, ()=> {
+  console.log('Listening on 3000!!!');
+});
