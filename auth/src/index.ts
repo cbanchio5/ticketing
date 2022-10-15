1c1a458298e2e289b52b11047ed0414205f86d28
cesar banchio <cbanchio5@gmail.com>
2022-10-15 16:56:02 +0200

    added mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
 import { errorHandler } from './middleware/error-handler';
-
+import { NotFoundError } from './errors/not-found-error';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -16,8 +18,23 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all('*', ()=> {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
-app.listen(3000, ()=> {
-  console.log('Listening on 3000!!!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
+
+start();
