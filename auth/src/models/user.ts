c06750b186c3cec20a74ee762e53156579d31dda
cesar banchio <cbanchio5@gmail.com>
2022-10-15 17:42:24 +0200

    created duplicated user error

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,23 @@
 import mongoose from 'mongoose';
 
+//Interface that describes properties for new user
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+//Interface that describes properties that User model has
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs) : UserDoc
+}
+
+//Interface that describes properties that User document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -13,6 +31,12 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+
 
 export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,7 +2,8 @@ import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser'
 import { body, validationResult } from 'express-validator'
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -14,21 +15,25 @@ router.post('/api/users/signup',[
   .trim()
   .isLength({min: 4, max: 20})
   .withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
   const errors = validationResult(req);
 
   if(!errors.isEmpty()) {
     throw new RequestValidationError(errors.array());
   }
 
-  const { email, passowrd } = req.body;
+  const { email, password } = req.body;
 
-  console.log('Creating user...');
+  const existingUser = await User.findOne({ email });
 
-  throw new DatabaseConnectionError();
+  if(existingUser) {
+    throw new BadRequestError('Email already in use');
+  }
 
-  res.send({});
+  const user = User.build({ email, password });
+  await user.save();
 
+  res.status(201).send({ user });
 
 });
 
