9bdb448a30680884fe0a4481444115dcb9529a4a
cesar banchio <cbanchio5@gmail.com>
2022-10-16 13:18:52 +0200

    added cookie session and jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,10 +8,16 @@ import { signoutRouter } from './routes/signout';
 import { errorHandler } from './middleware/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 import mongoose from 'mongoose';
+import cookieSession from "cookie-session";
 
 const app = express();
+app.set('trust proxy', true);
 
 app.use(json());
+app.use(cookieSession({
+  signed:false,
+  secure: true
+}));
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -33,7 +39,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
+    console.log('Listening on port 3000!');
   });
 };
 
