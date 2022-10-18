3a4d4812e82c038b19cd39d0c27555cd2f624f10
cesar banchio <cbanchio5@gmail.com>
2022-10-18 19:01:39 +0200

    implemented links in header

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,12 +1,33 @@
 import Link from 'next/link';
 export default ({currentUser}) => {
+  const links = [
+    !currentUser && {
+      label: 'Sign Up',
+      href: '/auth/signup'
+    },
+    !currentUser && {
+      label: 'Sign In',
+      href: '/auth/signin'
+    },
+    currentUser && {
+      label: 'Sign Out',
+      href: '/auth/signout'
+    }
+
+  ].filter(linkConfig => linkConfig).map(({label, href}) => {
+    return <li key={href} className='nav-item'>
+            <Link href={href}>
+              {label}
+            </Link>
+           </li>
+  });
   return <nav className='navbar navbar-light bg-light'>
           <Link  href='/'>
             <a className='navbar-brand'>GimmeTix</a>
           </Link>
           <div className='d-flex justify-content-end'>
             <ul className='nav d-flex align-items-center'>
-              {currentUser ? 'Signout' : 'Sign in/up'}
+             {links}
             </ul>
           </div>
          </nav>
