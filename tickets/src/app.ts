3653e36f4508546c401c66e6cb668702ed9d8a40
cesar banchio <cbanchio5@gmail.com>
2022-10-19 17:13:19 +0200

    created test for index route and implementation

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import { errorHandler, NotFoundError, currentUser } from '@cbanchio5tickets/comm
 import cookieSession from "cookie-session";
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,6 +19,7 @@ app.use(cookieSession({
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', ()=> {
   throw new NotFoundError();
