bb468cd59f510bdd886de8cb89ee839ba4499546
cesar banchio <cbanchio5@gmail.com>
2022-10-19 11:53:03 +0200

    changed files to look in common npm package

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middleware/current-user';
+import { currentUser } from '@cbanchio5tickets/common';
 
 const router = express.Router();
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,9 +2,8 @@ import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser'
 import { body } from 'express-validator'
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 import jwt from "jsonwebtoken";
-import { validateRequest } from '../middleware/validate-request';
+import { validateRequest, BadRequestError } from '@cbanchio5tickets/common';
 
 const router = express.Router();
 
