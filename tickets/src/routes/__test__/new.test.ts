d13112e26c4644d0d41f0f14b85ea571034f759b
cesar banchio <cbanchio5@gmail.com>
2022-10-19 16:20:02 +0200

    created ticket model

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -27,13 +27,53 @@ it('returns a status other than 401 is user is signed in', async()=> {
 });
 
 it('returns an error if invalid title is provided', async()=> {
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    title: '',
+    price: '12'
+  }).expect(400);
+
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    price: '12'
+  }).expect(400);
 
 });
 
 it('returns an error if invalid price is provided', async()=> {
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    title: 'title',
+    price: ''
+  }).expect(400);
+
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    title: 'title'
+  }).expect(400);
+
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    price: '-12'
+  }).expect(400);
 
 });
 
 it('creates a ticket for valid input', async()=> {
-
+  await request(app)
+  .post('/api/tickets')
+  .send({
+    title:'title',
+    price:'12'
+  }).expect(201)
 });
