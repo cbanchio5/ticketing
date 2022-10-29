cc0c6d9579a046ba2cc3c375f3df3eb7718e4b1f
cesar banchio <cbanchio5@gmail.com>
2022-10-29 18:22:50 +0200

    transfer classes to common module and updated version

--- nat-test/src/publisher.ts
+++ nat-test/src/publisher.ts
@@ -1,27 +1,33 @@
 import nats from 'node-nats-streaming';
-import { TicketCreatedPublisher} from './events/ticket-created-publisher';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
+
 const stan = nats.connect('ticketing', 'abc', {
-  url: 'https://localhost:4222'
+  url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
-  console.log("Publisher connected to NATS");
+stan.on('connect', async () => {
+  console.log('Publisher connected to NATS');
+
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
   // const data = JSON.stringify({
   //   id: '123',
   //   title: 'concert',
-  //   price: 20
+  //   price: '$20',
   // });
 
-  // stan.publish('ticket:created', data, () => {
-  //   console.log('Event published.');
+  // stan.publish('TicketCreated', data, () => {
+  //   console.log('Event published');
   // });
-
-  const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
-    id: '123',
-    title: 'concert',
-    price: 30
-  });
-})
+});
