895b04729702ecea973cb6b8d9a91dbd6edf2a3e
cesar banchio <cbanchio5@gmail.com>
2022-11-02 12:49:18 +0100

    created enum for orserstatus

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import { OrderStatus } from '@cbanchio5tickets/common';
 
 interface OrderAttrs {
 
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 
@@ -11,7 +12,7 @@ interface OrderAttrs {
 
 interface OrderDoc extends mongoose.Document {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 
@@ -28,7 +29,9 @@ const orderSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    required: true
+    required: true,
+    enum: Object.values(OrderStatus),
+    default: OrderStatus.Created
   },
   expiresAt: {
     type: mongoose.Schema.Types.Date
