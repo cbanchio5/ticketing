b7bb56e1dfea2091fd4179264bd35c8ff3801a09
cesar banchio <cbanchio5@gmail.com>
2022-11-02 12:26:40 +0100

    fixed model order error

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,16 @@
 import express, {Request, Response} from 'express';
-
+import { requireAuth, validateRequest } from '@cbanchio5tickets/common';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+  body('ticketId')
+  .not()
+  .isEmpty()
+  .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+  .withMessage('Ticket Id must be provided')
+], validateRequest,async (req: Request, res: Response) => {
   res.send({});
 });
 
