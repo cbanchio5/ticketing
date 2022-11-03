0dd46a62261339a093efc6fcf27ed5fa519ad1ec
cesar banchio <cbanchio5@gmail.com>
2022-11-03 12:31:57 +0100

    finished order model

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,10 @@
 import express, {Request, Response} from 'express';
-import { requireAuth, validateRequest } from '@cbanchio5tickets/common';
+import { NotFoundError, requireAuth, validateRequest, OrderStatus, BadRequestError } from '@cbanchio5tickets/common';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
+
 const router = express.Router();
 
 router.post('/api/orders', requireAuth, [
@@ -11,7 +14,30 @@ router.post('/api/orders', requireAuth, [
   .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
   .withMessage('Ticket Id must be provided')
 ], validateRequest,async (req: Request, res: Response) => {
-  res.send({});
+  //Find the ticket use is ordering
+
+  const { ticketId} = req.body;
+
+  const ticket = await Ticket.findById(ticketId);
+
+  if(!ticket) {
+    throw new NotFoundError();
+  }
+
+  const isReserved = await ticket.isReserved();
+
+  if(isReserved) {
+    throw new BadRequestError('Ticket is already reserved');
+  }
+
+
+  //Make sure ticket is not reserved
+
+  // Calculate expiration date
+
+  //Build order and save to database
+
+  //Publish event of ordering created
 });
 
 export {router as newOrderRouter};
