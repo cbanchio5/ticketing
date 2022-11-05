7440a280ca7fa6a387d34a544c339b76a9551994
cesar banchio <cbanchio5@gmail.com>
2022-11-05 10:15:06 +0100

    created delete route handler and test

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,29 @@
 import express, {Request, Response} from 'express';
+import { Order, OrderStatus } from '../models/order';
+import { requireAuth, NotAuhtorizeError, NotFoundError } from '@cbanchio5tickets/common';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+
+  const { orderId } = req.params;
+
+  const order = await Order.findById(orderId);
+
+  if(!order){
+    throw new NotFoundError()
+  }
+
+  if(order.userId !== req.currentUser!.id) {
+    throw new NotAuhtorizeError();
+  }
+
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  //publish an event saying order was cancelled
+
+  res.status(204).send(order);
 });
 
 export {router as deleteOrderRouter}
