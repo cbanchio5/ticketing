b56b25bfa866ce395d7b021952aec6a8e3f252ef
cesar banchio <cbanchio5@gmail.com>
2022-11-19 10:45:29 +0100

    added order created listener in expiration service

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,5 +1,6 @@
 
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
 
@@ -24,6 +25,7 @@ const start = async () => {
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
+    new OrderCreatedListener(natsWrapper.client).listen();
   } catch (err) {
     console.error(err);
   }
