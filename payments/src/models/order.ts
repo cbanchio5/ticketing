fe05577387dbfc3fd628c7a295e798b92c7a651f
cesar banchio <cbanchio5@gmail.com>
2022-11-22 19:46:07 +0100

    created payments service

---
+++ payments/src/models/order.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+interface OrderAttrs {
+
+}
+
+interface OrderDoc extends mongoose.Document {
+
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+
+}
