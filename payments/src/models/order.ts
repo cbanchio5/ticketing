b6169cdfd719e587d8dd93813db86d4f4da5124c
cesar banchio <cbanchio5@gmail.com>
2022-11-22 20:56:08 +0100

    created order cancelled listener in payments and tests

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -1,15 +1,13 @@
 import mongoose from 'mongoose';
-import {OrderStatus} from '@cbanchio5tickets/common'
-import {updateIfCurrentPlugin} from 'mongoose-update-if-current';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+import { OrderStatus } from "@cbanchio5tickets/common"
 
 interface OrderAttrs {
-
   id: string;
   version: number;
   userId: string;
-  price:number;
+  price: number;
   status: OrderStatus;
-
 }
 
 interface OrderDoc extends mongoose.Document {
@@ -17,39 +15,38 @@ interface OrderDoc extends mongoose.Document {
   userId: string;
   price: number;
   status: OrderStatus;
-
 }
 
 interface OrderModel extends mongoose.Model<OrderDoc> {
-
-  build(attrs: OrderAttrs) : OrderDoc;
+  build(attrs: OrderAttrs): OrderDoc;
 }
 
-const orderSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
   },
-  price: {
-    type: Number,
-    required: true
-  },
-  status: {
-    type: String,
-    required: true
-  }
-}, {
+  {
     toJSON: {
       transform(doc, ret) {
         ret.id = ret._id;
         delete ret._id;
-      }
-    }
-
-});
+      },
+    },
+  }
+);
 
 orderSchema.set('versionKey', 'version');
-
 orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
@@ -58,11 +55,10 @@ orderSchema.statics.build = (attrs: OrderAttrs) => {
     version: attrs.version,
     price: attrs.price,
     userId: attrs.userId,
-    status: attrs.status
-
-  })
-}
+    status: attrs.status,
+  });
+};
 
 const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
 
-export {Order};
+export { Order };
