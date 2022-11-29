29a232a6ff6d7bf4b69d7f796aae7157963b1fb6
cesar banchio <cbanchio5@gmail.com>
2022-11-29 15:50:06 +0100

    solved prop type error stripe checkout module

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,28 +1,45 @@
-import {useState, useEffect} from 'react';
+import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
-const OrderShow = ({order}) => {
-
-  const [timeLeft, setTimeLeft] = useState('');
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
-    const findTimeLeft = ()=> {
+    const findTimeLeft = () => {
       const msLeft = new Date(order.expiresAt) - new Date();
-      setTimeLeft(Math.round(msLeft/1000))
-    }
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
     findTimeLeft();
     const timerId = setInterval(findTimeLeft, 1000);
 
-    return() => {
+    return () => {
       clearInterval(timerId);
-    }
-  }, [])
+    };
+  }, [order]);
 
-  return <div>Time left to pay: {timeLeft}</div>
-}
-OrderShow.getInitialProps = async(context, client) => {
-  const {orderId} = context.query;
-  const {data} = await client.get(`/api/orders/${orderId}`);
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
 
-  return {order:data};
-}
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        stripeKey="pk_test_51M7ksBD6fqnqMwy8NygxqVYxj5C0hpTNRnOFFJ8qQv2zRf5Qv9BdH4HXiwYrGHWvTjdvo7QjC5XNBcDVYL7LzWZe00Eof7WzDw"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+    </div>
+  );
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
 export default OrderShow;
