730947110614a35a1f78e5e3b2c570251c0d10ce
cesar banchio <cbanchio5@gmail.com>
2022-11-29 15:30:00 +0100

    added time left to pay timer on order show route

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,28 @@
-const OrderShow = () => {
-  return <div>OrderShow</div>
-}
+import {useState, useEffect} from 'react';
 
+const OrderShow = ({order}) => {
+
+  const [timeLeft, setTimeLeft] = useState('');
+
+  useEffect(() => {
+    const findTimeLeft = ()=> {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft/1000))
+    }
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return() => {
+      clearInterval(timerId);
+    }
+  }, [])
+
+  return <div>Time left to pay: {timeLeft}</div>
+}
+OrderShow.getInitialProps = async(context, client) => {
+  const {orderId} = context.query;
+  const {data} = await client.get(`/api/orders/${orderId}`);
+
+  return {order:data};
+}
 export default OrderShow;
